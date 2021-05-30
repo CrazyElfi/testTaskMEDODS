83c1543d3bfba666f849fe67811f6c7312c7cbad
elfi <alrigaff@gmail.com>
2021-05-30 08:27:57 +0300

    deploy script

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     // options...
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/'
+        ? '/testTaskMEDODS/'
         : '/',
     assetsDir: '',
     productionSourceMap: false,
